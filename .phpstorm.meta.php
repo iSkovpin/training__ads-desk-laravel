5218b6fb401f256389bdba1e37918ebff068be12
Ivan Skovpin <ivanskovpin@gmail.com>
2020-02-25 18:01:58 +0700

    + laravel ui

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-25 10:39:51.
+    * Generated on 2020-02-25 11:01:28.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
