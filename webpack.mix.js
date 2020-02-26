1af80d9bb7013ec476630cb9bc283a398a02ef64
Ivan Skovpin <ivanskovpin@gmail.com>
2020-02-26 22:35:10 +0700

    + assets version

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.setPublicPath('public/build')
-    .setResourceRoot('build')
     .js('resources/js/app.js', 'js')
-    .sass('resources/sass/app.scss', 'css');
+    .sass('resources/sass/app.scss', 'css')
+    .version();
